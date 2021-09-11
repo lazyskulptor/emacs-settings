edc2c6b4f5ae6841956aa6b7512e5b42f4b3e6fa
Park Hyeonjun <phjosh06@gmail.com>
2021-09-11 22:20:12 +0900

    lsp-java installed

--- config/lang-modes.el
+++ config/lang-modes.el
@@ -3,14 +3,13 @@
 (use-package js2-mode :ensure t)
 (use-package json-mode :ensure t)
 (use-package cider :ensure t)
+;; (use-package java-mode :ensure t)
 
  ;;     lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
 (add-hook 'clojure-mode-hook
-      (lambda ()
-        (make-local-variable 'lsp-enable-completion-at-point)
-        (setq 
-	  lsp-enable-completion-at-point nil
-          lsp-enable-indentation nil)))
+          (lambda ()
+            (make-local-variable 'lsp-enable-completion-at-point)
+            (setq lsp-enable-completion-at-point nil lsp-enable-indentation nil)))
 
 ;;  end of file
 (provide 'lang-modes)
